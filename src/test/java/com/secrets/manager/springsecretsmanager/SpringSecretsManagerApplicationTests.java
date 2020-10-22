a5e3192048db306855a4e2552c46016c088c3994
Viyaan Jhiingade <30381782+Viyaan@users.noreply.github.com>
2020-10-22 17:42:22 +0530

    add files

---
+++ src/test/java/com/secrets/manager/springsecretsmanager/SpringSecretsManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secrets.manager.springsecretsmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecretsManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
